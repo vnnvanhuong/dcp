65649453404a5b4bb5ca2799c6218b0ff331929f
Huong Nguyen <huong.nguyen@ubitec.ch>
2019-04-21 22:34:29 +0700

    problem #2 | optimize

--- problem_2/solution.py
+++ problem_2/solution.py
@@ -1,4 +1,9 @@
 def newArr(arr):
+    """
+    MapReduce: O(n^2)
+    1. Map: construct the Map with Key is each element and Value is the array
+    2. Reduce: product all element of Value except the key
+    """
 
     result = []
     hashmap = {}
@@ -11,5 +16,30 @@ def newArr(arr):
             if (v != key):
                 product *=v
         result.append(product)
+    
+    return result
+
+
+def optimizedNewArr(arr):
+    """
+    Ref: https://www.geeksforgeeks.org/a-product-array-puzzle/ O(n)
+    1) Construct a temporary array left[] such that left[i] contains product of all elements on left of arr[i] excluding arr[i].
+    2) Construct another temporary array right[] such that right[i] contains product of all elements on on right of arr[i] excluding arr[i].
+    3) To get prod[], multiply left[] and right[].
+    """
+    result = []
+
+    
+    temp = 1
+    for i in range(len(arr)):
+        result.append(temp)
+        temp *= arr[i]
+    
+    temp = 1
+    j = len(arr) - 1
+    while (j >= 0):
+        result[j] *= temp
+        temp *= arr[j]
+        j -= 1
     
     return result
--- problem_2/test.py
+++ problem_2/test.py
@@ -1,5 +1,5 @@
 import unittest
-from problem_2.solution import newArr
+from problem_2.solution import newArr, optimizedNewArr
 
 class TestProblem2(unittest.TestCase):
 
@@ -8,6 +8,13 @@ class TestProblem2(unittest.TestCase):
     
     def test_case_2(self):
         self.assertEquals(newArr([3, 2, 1]), [2, 3, 6])
+
+
+    def test_case_3(self):
+        self.assertEqual(optimizedNewArr([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
+    
+    def test_case_4(self):
+        self.assertEquals(optimizedNewArr([3, 2, 1]), [2, 3, 6])
 
 if __name__ == '__main__':
     unittest.main()
